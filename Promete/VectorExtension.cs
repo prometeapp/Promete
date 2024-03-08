b5c60622a7337ad0de881d6f93f88c810f711cf8
Ebise Lutica <7106976+EbiseLutica@users.noreply.github.com>
2024-03-09 04:46:39 +0900

    VectorExtension クラスを追加

---
+++ Promete/VectorExtension.cs
@@ -0,0 +1,12 @@
+﻿namespace Promete;
+
+public static class VectorExtension
+{
+	public static System.Numerics.Vector2 ToNumerics(this VectorInt vector) => new(vector.X, vector.Y);
+
+	public static VectorInt ToPrometeInt(this System.Numerics.Vector2 vector) => new((int)vector.X, (int)vector.Y);
+
+	public static System.Numerics.Vector2 ToNumerics(this Vector vector) => new(vector.X, vector.Y);
+
+	public static Vector ToPromete(this System.Numerics.Vector2 vector) => new(vector.X, vector.Y);
+}
