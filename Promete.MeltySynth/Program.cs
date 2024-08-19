d257d039b2a692bcbab56a047560434d77cf684b
Ebise Lutica <7106976+EbiseLutica@users.noreply.github.com>
2024-08-20 02:47:38 +0900

    Add a example to integrate with MeltySynth

---
+++ Promete.MeltySynth/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MeltySynth;
+using Promete;
+using Promete.Audio;
+using Promete.GLDesktop;
+using Promete.MeltySynth;
+
+
+var app = PrometeApp.Create()
+	.BuildWithOpenGLDesktop();
+
+return app.Run<MainScene>();
+
+public class MainScene : Scene
+{
+	public override void OnStart()
+	{
+		var audioPlayer = new AudioPlayer();
+		var source = new MeltySynthAudioSource("./soundfont.sf2");
+		source.Play(new MidiFile("song.mid", MidiFileLoopType.RpgMaker), true);
+
+		audioPlayer.BufferSize = 4000;
+		audioPlayer.Play(source);
+	}
+}
---
+++ Promete.MeltySynth/README.md
@@ -0,0 +1,8 @@
+﻿# Promete.MeltySynth
+
+PrometeでMeltySynthを使うサンプルコード。
+
+DotFeather向けのサンプルコードをベースにしています。
+
+https://github.com/sinshu/meltysynth/tree/main/Examples/DotFeather
+
