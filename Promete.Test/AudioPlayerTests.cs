e274efdc3d66569a7fe14dfd67a7c66c5ef4f8e0
Ebise Lutica <7106976+EbiseLutica@users.noreply.github.com>
2024-12-19 14:48:29 +0900

    test: AudioPlayerのテストを追加

---
+++ Promete.Test/AudioPlayerTests.cs
@@ -0,0 +1,79 @@
+﻿using FluentAssertions;
+using Promete.Audio;
+
+namespace Promete.Test;
+
+public class AudioPlayerTests
+{
+    [Fact]
+    public void VorbisAudioSource_CanLoad()
+    {
+        var initialize = () => new VorbisAudioSource("./assets/GB-Action-C02-2.ogg");
+
+        initialize.Should().NotThrow();
+    }
+
+    [Fact]
+    public void PlayAndStop()
+    {
+        using var audioPlayer = new AudioPlayer();
+        using var audioSource = new VorbisAudioSource("./assets/GB-Action-C02-2.ogg");
+
+        audioPlayer.Invoking(x => x.Play(audioSource)).Should().NotThrow();
+        audioPlayer.IsPlaying.Should().BeTrue();
+
+        audioPlayer.Stop();
+        audioPlayer.IsPlaying.Should().BeFalse();
+    }
+
+    [Fact]
+    public void PauseAndResume()
+    {
+        using var audioPlayer = new AudioPlayer();
+        using var audioSource = new VorbisAudioSource("./assets/GB-Action-C02-2.ogg");
+
+        audioPlayer.Play(audioSource);
+        audioPlayer.IsPausing.Should().BeFalse();
+
+        audioPlayer.Pause();
+        audioPlayer.IsPlaying.Should().BeTrue();
+        audioPlayer.IsPausing.Should().BeTrue();
+
+        audioPlayer.Resume();
+        audioPlayer.IsPlaying.Should().BeTrue();
+        audioPlayer.IsPausing.Should().BeFalse();
+
+        audioPlayer.Stop();
+    }
+
+    [Fact]
+    public void PauseAndStop()
+    {
+        using var audioPlayer = new AudioPlayer();
+        using var audioSource = new VorbisAudioSource("./assets/GB-Action-C02-2.ogg");
+
+        audioPlayer.Play(audioSource);
+        audioPlayer.IsPausing.Should().BeFalse();
+
+        audioPlayer.Pause();
+        audioPlayer.IsPlaying.Should().BeTrue();
+        audioPlayer.IsPausing.Should().BeTrue();
+
+        audioPlayer.Stop();
+        audioPlayer.IsPlaying.Should().BeFalse();
+        audioPlayer.IsPausing.Should().BeFalse();
+    }
+
+    [Fact]
+    public void IsPlayingShouldBeTrueWhenPlayTwice()
+    {
+        using var audioPlayer = new AudioPlayer();
+        using var audioSource = new VorbisAudioSource("./assets/GB-Action-C02-2.ogg");
+
+        audioPlayer.Play(audioSource);
+        audioPlayer.Play(audioSource);
+        audioPlayer.IsPlaying.Should().BeTrue();
+
+        audioPlayer.Stop();
+    }
+}
