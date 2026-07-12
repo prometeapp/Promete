f57e3f05516aa6e2427793123cac3465c52ff710
Ebise Lutica <7106976+EbiseLutica@users.noreply.github.com>
2026-07-12 12:27:59 +0900

    テストの修正

--- Promete.Test/ScenelessRunTests.cs
+++ Promete.Test/ScenelessRunTests.cs
@@ -1,64 +1,30 @@
-using System.Reflection;
 using FluentAssertions;
 using Promete.Headless;
+using Promete.Nodes;
 
 namespace Promete.Test;
 
 public class ScenelessRunTests
 {
-    /// <summary>
-    /// DefaultScene の型を取得するヘルパーメソッド
-    /// </summary>
-    private static Type GetDefaultSceneType()
-    {
-        return typeof(PrometeApp).GetNestedType("DefaultScene", BindingFlags.NonPublic)!;
-    }
-
     [Fact]
-    public void Run_WithoutScene_ShouldHaveValidRoot()
+    public void Run_WithoutScene_ShouldStartUpdateAndExit()
     {
-        // Arrange
         using var app = PrometeApp.Create().BuildWithHeadless();
 
-        // OnStartを手動で呼び出して初期化し、DefaultSceneをロード
-        app.OnStart();
-        app.LoadScene(GetDefaultSceneType());
+        Container? rootAtStart = null;
+        var updated = false;
 
-        // Assert
-        app.Root.Should()
-            .NotBeNull("Root container should be initialized even without explicit scene");
-    }
+        app.Start += () => rootAtStart = app.Root;
+        app.Update += () =>
+        {
+            updated = true;
+            app.Exit();
+        };
 
-    [Fact]
-    public void GetPlugin_ShouldWork_WhenRunningWithoutScene()
-    {
-        // Arrange
-        using var app = PrometeApp.Create().Use<ConsoleLayer>().BuildWithHeadless();
+        var status = app.Run();
 
-        // Act
-        var console = app.GetPlugin<ConsoleLayer>();
-
-        // Assert
-        console
-            .Should()
-            .NotBeNull("Plugins should be accessible even when running without explicit scene");
-    }
-
-    [Fact]
-    public void DefaultScene_ShouldBeRegistered()
-    {
-        // Arrange
-        using var app = PrometeApp.Create().BuildWithHeadless();
-
-        // Act - DefaultSceneのロードを試みる
-        var loadSceneMethod = typeof(PrometeApp).GetMethod(
-            "LoadScene",
-            BindingFlags.Public | BindingFlags.Instance,
-            [typeof(Type)]
-        )!;
-
-        // Assert - 例外がスローされないことを確認
-        var act = () => loadSceneMethod.Invoke(app, [GetDefaultSceneType()]);
-        act.Should().NotThrow("DefaultScene should be registered and loadable");
+        status.Should().Be(0, "Exit() の既定ステータスコードで終了するはず");
+        rootAtStart.Should().NotBeNull("シーンを指定しなくても Root が初期化されるはず");
+        updated.Should().BeTrue("シーンなしでも更新ループが回るはず");
     }
 }
